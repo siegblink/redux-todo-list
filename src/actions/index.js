4ddb6b058593848e8099b22d786f5f6dd967240f
Siegfred Balona <sbalona@qualfon.com>
2018-12-06 13:14:06 -0500

    Fix indentation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 // Action creators
 export const addTodo = todo => {
-         return { type: 'ADD_TODO', payload: todo };
-       };
+  return { type: 'ADD_TODO', payload: todo };
+};
 
 export const clearTodo = todo => {
   return { type: 'CLEAR_TODO', payload: todo };
